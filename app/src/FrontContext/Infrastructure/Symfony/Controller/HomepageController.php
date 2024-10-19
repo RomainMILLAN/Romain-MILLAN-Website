ac50fc061d14dd3c4b20d12514bb24326ac5c114
Romain Millan <romainmillan34560@gmail.com>
2024-10-19 10:28:21 +0200

    feat(portoflio): change translation domain from app to front

--- app/src/FrontContext/Infrastructure/Symfony/Controller/HomepageController.php
+++ app/src/FrontContext/Infrastructure/Symfony/Controller/HomepageController.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace App\FrontContext\Infrastructure\Symfony\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(
     path: '/',
     name: RouteCollection::HOMEPAGE->value,
-    methods: ['GET'],
+    methods: [Request::METHOD_GET],
 )]
 class HomepageController extends AbstractController
 {
--- app/src/FrontContext/Infrastructure/Symfony/Controller/PortfolioController.php
+++ app/src/FrontContext/Infrastructure/Symfony/Controller/PortfolioController.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace App\FrontContext\Infrastructure\Symfony\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(
     path: '/portfolio',
     name: RouteCollection::PORTFOLIO->value,
-    methods: ['GET'],
+    methods: [Request::METHOD_GET],
 )]
 class PortfolioController extends AbstractController
 {
