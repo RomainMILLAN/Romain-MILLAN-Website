57e48723291d4674bf022eb0a8aa4d1659a200cb
Romain Millan <romainmillan34560@gmail.com>
2024-10-20 09:01:10 +0200

    feat(panel): Dashboard + Uptime Robot + Uptime Kuma

---
+++ app/src/PanelContext/Infrastructure/Symfony/Controller/CUptimeRobotController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\PanelContext\Infrastructure\Symfony\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(
+    path: '/c-uptime-robot',
+    name: RouteCollection::CLOUD_UPTIME_ROBOT->value,
+    methods: [Request::METHOD_GET],
+)]
+class CUptimeRobotController extends AbstractController
+{
+    public function __invoke(): Response
+    {
+        return $this->render(
+            view: 'panel/cloud_uptime_robot.html.twig',
+        );
+    }
+}
--- app/src/PanelContext/Infrastructure/Symfony/Controller/RouteCollection.php
+++ app/src/PanelContext/Infrastructure/Symfony/Controller/RouteCollection.php
@@ -10,4 +10,6 @@ enum RouteCollection: string implements RouteCollectionInterface
     use AppPanelContextRouteCollectionTrait;
 
     case DASHBOARD = 'dashboard';
+    case PROD_01_UPTIME_KUMA = 'prod_01_uptime_kuma';
+    case CLOUD_UPTIME_ROBOT = 'cloud_uptime_robot';
 }
