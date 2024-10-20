f1bc1dd93f759923d7884ec64166a301e7f850a2
Romain Millan <romainmillan34560@gmail.com>
2024-10-20 09:14:00 +0200

    feat(panel): dockge iframe

---
+++ app/src/PanelContext/Infrastructure/Symfony/Controller/P1DockgeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\PanelContext\Infrastructure\Symfony\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(
+    path: '/p01-dockge',
+    name: RouteCollection::PROD_01_DOCKGE->value,
+    methods: [Request::METHOD_GET],
+)]
+class P1DockgeController extends AbstractController
+{
+    public function __invoke(): Response
+    {
+        return $this->render(
+            view: 'panel/prod_01_dockge.html.twig',
+        );
+    }
+}
--- app/src/PanelContext/Infrastructure/Symfony/Controller/RouteCollection.php
+++ app/src/PanelContext/Infrastructure/Symfony/Controller/RouteCollection.php
@@ -12,4 +12,5 @@ enum RouteCollection: string implements RouteCollectionInterface
     case DASHBOARD = 'dashboard';
     case PROD_01_UPTIME_KUMA = 'prod_01_uptime_kuma';
     case CLOUD_UPTIME_ROBOT = 'cloud_uptime_robot';
+    case PROD_01_DOCKGE = 'prod_01_dockge';
 }
