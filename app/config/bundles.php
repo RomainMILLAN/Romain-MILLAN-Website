a5dc4156885aec96d939b781c9660069e780ba10
Romain Millan <romainmillan34560@gmail.com>
2024-12-25 18:15:47 +0100

    feat(doctrine): Remove dependencies not used

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -4,12 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => [
         'all' => true,
     ],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => [
-        'all' => true,
-    ],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => [
-        'all' => true,
-    ],
     Symfony\Bundle\DebugBundle\DebugBundle::class => [
         'dev' => true,
     ],
