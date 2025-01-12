5bafbd74757f847f5ba668c98c20b0c3c43dac79
Romain Millan <romainmillan34560@gmail.com>
2025-01-12 18:32:47 +0100

    Switch light/dark mode

---
+++ app/assets/front/controllers/light-mode_controller.ts
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["light", "dark", "body"];
+
+  declare readonly lightTarget: HTMLInputElement;
+  declare readonly darkTarget: HTMLInputElement;
+  declare readonly bodyTarget: HTMLInputElement;
+
+  connect() {
+    console.log("LightMode controller connected");
+  }
+
+  public switchToLightMode(): void {
+    this.darkTarget.style.display = 'flex';
+    this.lightTarget.style.display = 'none';
+    this.bodyTarget.classList.remove('dark-mode');
+  }
+
+  public switchToDarkMode(): void {
+    this.darkTarget.style.display = 'none';
+    this.lightTarget.style.display = 'flex';
+    this.bodyTarget.classList.add('dark-mode');
+  }
+
+}
