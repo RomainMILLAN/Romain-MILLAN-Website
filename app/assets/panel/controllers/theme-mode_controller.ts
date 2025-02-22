2871f85212aed7eb3c86b0375150f27255430d9e
Romain Millan <romainmillan34560@gmail.com>
2025-02-22 14:32:18 +0100

    feat: Adding theme mode for panel

---
+++ app/assets/panel/controllers/theme-mode_controller.ts
@@ -0,0 +1,41 @@
+import { Controller } from "@hotwired/stimulus";
+
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+  static targets = ["body"];
+
+  declare readonly bodyTarget: HTMLInputElement;
+
+  connect() {
+    console.log("ThemeMode controller connected 💡");
+
+    this.detectThemeChange();
+  }
+
+  public switchToLightMode(): void {
+    this.bodyTarget.setAttribute('data-bs-theme', 'light')
+  }
+
+  public switchToDarkMode(): void {
+    this.bodyTarget.setAttribute('data-bs-theme', 'dark')
+  }
+
+  public detectThemeChange(): void {
+    const darkModeMediaQuery: MediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+
+    if (darkModeMediaQuery.matches) {
+      this.switchToDarkMode();
+    } else {
+      this.switchToLightMode()
+    }
+
+    darkModeMediaQuery.addEventListener('change', (event: MediaQueryListEvent): void => {
+      if (event.matches) {
+        this.switchToDarkMode();
+      } else {
+        this.switchToLightMode()
+      }
+    });
+  }
+
+}
