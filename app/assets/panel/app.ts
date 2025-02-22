82cd2029f0a0b9e5560f416c4d19b5a47d3a81a7
Romain Millan <romainmillan34560@gmail.com>
2025-02-22 14:25:45 +0100

    feat: Dashboard

--- app/assets/panel/app.ts
+++ app/assets/panel/app.ts
@@ -1,12 +1,7 @@
 import './bootstrap.ts';
-
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 import './styles/app.scss';
 
-import '../../public/bundles/tabler/tabler.js';
-import '../rm-tag/tag.css';
+// import '../rm-tag/tag.css';
+// import '@tabler/core';
+import '@tabler/core/dist/js/tabler.min.js';
+import '@tabler/core/dist/css/tabler.min.css';
