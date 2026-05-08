5ada2cdc89aa011346095fc941ea0e726aeb9870
Romain Millan <romainmillan34560@gmail.com>
2026-05-08 18:06:45 +0200

    fix(panel): Use node module tabler and not CDN

--- assets/panel/app.ts
+++ assets/panel/app.ts
@@ -1,4 +1,6 @@
 import './bootstrap.ts';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@tabler/core/dist/css/tabler.min.css';
 import './styles/app.scss';
 
 // import '../rm-tag/tag.css';
